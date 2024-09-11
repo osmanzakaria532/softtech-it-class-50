4e07568cfd0abd97e79d1f9f17a56b3fde43c2d0
osmanzakaria532 <osmanzakaria179@gmail.com>
2024-09-11 14:07:15 +0600

    class-50 > 1.6 is finish > add indivisible header.php, footer.php and custom-contents-parts functionality

--- wp-content/themes/firsttheme/index.php
+++ wp-content/themes/firsttheme/index.php
@@ -1,57 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- <title><?php bloginfo()?></title> -->
 
-        <!-- dynamic title add process  -->
-        <?php wp_head() ?>
-        <link rel="stylesheet" href="style.css">
-        <style>
-            .banner-area img {
-                width: 600px;
-                margin: 0 auto;
-            }
-        </style>
-    </head>
-    <body <?php body_class(); ?>>
+<?php 
+    // require_once('header.php'); 
+    get_header(); 
+?>
 
-        <h1>welcome to our website</h1>
-
-        <div class="banner-area">
-            <!-- <img src="<?php echo get_template_directory_urI() ?>/images/banner.jpg" alt=""> -->
-            <img src="<?php header_image() ?>" alt="">
-        </div>
-
-        <!-- <nav>
-            <?php
-                // show the menu in frontend
-                wp_nav_menu(
-                    // decided to which menu is visible
-                    array(
-                        'theme_location' => 'main-menu',
-                    )
-                );
-            ?>
-        </nav> -->
+<?php get_template_part('template-parts/content'); ?>
 
 
-        <!-- <footer>
-            <?php
-                // show the menu in frontend
-                wp_nav_menu(
-                    // decided to which menu is visible
-                    array(
-                        'theme_location' => 'footer-menu',
-                    )
-                );
-            ?>
 
-        </footer> -->
-        
-        <?php wp_footer(); ?>
-    </body>
-</html>
+
+<?php 
+    // require_once('footer.php');  
+    get_footer(); 
+?>
+
+
+
+
 
 
